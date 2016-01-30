b5859c087fc1604360afa9a4dc19191440ed912b
Sebastiaan Grasdijk <s.grasdijk@gmail.com>
2016-01-30 18:21:42 +0100

    V1.0 Report sebas.

--- Assignment4/rCode/VisualErrors.R
+++ Assignment4/rCode/VisualErrors.R
@@ -1,11 +1,12 @@
 data = read.table('trainingValidTest.txt')
+data = data*10
 
 range = c(1:200)
 
-plot(range, data[1,], type='l', col="red", ylim=c(0,1), ann=FALSE)
+plot(range, data[1,], type='l', col="red", ylim=c(0,100), ann=FALSE)
 lines(range, data[2,], col="green")
 lines(range, data[3,], col="black")
-title('Training, validation and test error for CIFAR', xlab='Epochs', ylab='Error(in %)')
+title('Training, validation and test error for a CIFAR dataset with LCN', xlab='Epochs', ylab='Error(in %)')
 name_errors = c('Training error', 'Validation error', 'Test error')
 
 legend('topright', name_errors, cex=0.8, col=c('red','green','black'), lty=1);
